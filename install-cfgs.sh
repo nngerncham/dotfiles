3704ede8fef9348a8ffaf18bbf1abba068512eb4
nawat <n.ngerncham@gmail.com>
2023-06-09 17:48:52 +0700

    tmux: moved to a more civilized method

--- install-cfgs.sh
+++ install-cfgs.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+if [ -d "~/.tmux/plugins/tpm" ]; then
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+fi
+
 cp -r dotconfig/* ~/.config/
-cp -r tohome/.tmux.conf ~/
