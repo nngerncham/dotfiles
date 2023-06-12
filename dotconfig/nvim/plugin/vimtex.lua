7fac9fdf23636f64d4d87d06ff5c6897e2b91558
Nawat Ngerncham <n.ngerncham@gmail.com>
2023-06-12 23:05:46 +0700

    alacritty: font change, nvim: vimtex settings

--- dotconfig/nvim/plugin/vimtex.lua
+++ dotconfig/nvim/plugin/vimtex.lua
@@ -5,8 +5,22 @@ vim.g.tex_flavor = "latex"
 vim.g.vimtex_view_method = "zathura"
 vim.g.vimtex_quickfix_mode = 0
 
-vim.cmd[[set conceallevel=1]]
-vim.g.tex_conceal = "abdmg"
+vim.cmd([[set conceallevel=2]])
+vim.cmd([[let g:vimtex_syntax_conceal = {
+          \ 'accents': 1,
+          \ 'ligatures': 1,
+          \ 'cites': 1,
+          \ 'fancy': 1,
+          \ 'spacing': 1,
+          \ 'greek': 1,
+          \ 'math_bounds': 1,
+          \ 'math_delimiters': 1,
+          \ 'math_fracs': 0,
+          \ 'math_super_sub': 0,
+          \ 'math_symbols': 1,
+          \ 'sections': 1,
+          \ 'styles': 1,
+          \}]])
 
 vim.g.vimtex_compiler_method = "latexmk"
 vim.g.Tex_DefaultTargetFormat = "pdf"
