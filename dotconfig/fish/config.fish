b5a5603184cd931bc84cfe062aa63e70e477b509
Nawat Ngerncham <n.ngerncham@gmail.com>
2023-08-22 14:17:09 +0700

    updated fish cfg on Linux

--- dotconfig/fish/config.fish
+++ dotconfig/fish/config.fish
@@ -3,16 +3,31 @@ if status is-interactive
 end
 
 alias nv="nvim"
+export EDITOR="nvim"
 
-# MacOS specific
+# Linux stuff
 if [ $(uname) != "Darwin" ]
     alias hx="helix"
+
+    export PYENV_ROOT="/home/nawat/.pyenv"
+    pyenv init - | source
+
+    export NVM_DIR="$HOME/.nvm"
+    fish_add_path "/home/nawat/.local/share/JetBrains/Toolbox/scripts"
+    fish_add_path "/home/nawat/.tmuxifier/bin"
+    fish_add_path "/home/nawat/.local/bin"
+    fish_add_path "/usr/local/texlive/2023/bin/x86_64-linux"
+    fish_add_path "/home/nawat/.pyenv/bin"
+
+# MacOS stuff
 else 
-    fish_add_path "/Users/nawat/.cargo/bin"
-    fish_add_path "/Users/nawat/Library/Application\ Support/JetBrains/Toolbox/scripts"
     fish_add_path "/opt/homebrew/bin"
-    fish_add_path "Users/nawat/.local/bin"
+    fish_add_path "/Users/nawat/.cargo/bin"
+    fish_add_path "/Users/nawat/.local/bin"
+
+    fish_add_path "/Users/nawat/Library/Application\ Support/JetBrains/Toolbox/scripts"
     fish_add_path "/Users/nawat/Library/Application Support/pypoetry/venv/bin"
+
     fish_add_path "/Users/nawat/.local/share/nvm/v20.5.1/bin/node"
 end
 
