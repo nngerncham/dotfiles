d6b9f514207a644307a1787df0910d5e68049e70
Nawat Ngerncham <67263644+nngerncham@users.noreply.github.com>
2024-01-05 00:15:20 +0700

    fish stuff

--- dotconfig/fish/config.fish
+++ dotconfig/fish/config.fish
@@ -2,43 +2,41 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-alias nv="lvim"
+alias lv="lvim"
+alias nv="nvim"
 export EDITOR="lvim"
 
-# Linux stuff
-if [ $(uname) != "Darwin" ]
+fish_add_path "$HOME/.tmuxifier/bin"
+fish_add_path "$HOME/.local/bin"
+fish_add_path "$HOME/.cargo/bin"
+
+# if venv not set, then add pyenv to path
+if test -z "$VIRTUAL_ENV"
+    export PYENV_ROOT="$HOME/.pyenv"
+    fish_add_path "$HOME/.pyenv/bin"
+    pyenv init - | source
+end
+
+if [ $(uname) != Darwin ]
+    # Linux stuff
     alias hx="helix"
 
     export NVM_DIR="$HOME/.nvm"
     fish_add_path "/home/nawat/.local/share/JetBrains/Toolbox/scripts"
-    fish_add_path "/home/nawat/.tmuxifier/bin"
-    fish_add_path "/home/nawat/.local/bin"
-    fish_add_path "/usr/local/texlive/2023/bin/x86_64-linux"
-    fish_add_path "/home/nawat/go/bin"
+    fish_add_path /usr/local/texlive/2023/bin/x86_64-linux
+    fish_add_path /home/nawat/go/bin
 
     export XDG_DATA_DIRS="/var/lib/flatpak/exports/share:/home/nawat/.local/share/flatpak/exports/share:$XDG_DATA_DIRS"
-    
-    # if venv not set, then add pyenv to path
-    if test -z "$VIRTUAL_ENV"
-        export PYENV_ROOT="/home/nawat/.pyenv"
-        fish_add_path "/home/nawat/.pyenv/bin"
-        pyenv init - | source
-    end
 
-# MacOS stuff
-else 
+else
+    # MacOS stuff
     export PYENV_ROOT="/Users/nawat/.pyenv"
 
-    fish_add_path "/opt/homebrew/bin"
+    fish_add_path /opt/homebrew/bin
     fish_add_path "$(brew --prefix)/opt/coreutils/libexec/gnubin"
-    fish_add_path "/Users/nawat/.cargo/bin"
-    fish_add_path "/Users/nawat/.local/bin"
 
     fish_add_path "/Users/nawat/Library/Application\ Support/JetBrains/Toolbox/scripts"
-
     fish_add_path "/Users/nawat/.local/share/nvm/v20.6.1/bin"
-
-    pyenv init - | source
 end
 
 starship init fish | source
