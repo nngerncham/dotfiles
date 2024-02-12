94e6ee2e2b62e17f19e9b6f9a64ebcd9d46ba156
Nawat Ngerncham <n.ngerncham@gmail.com>
2024-02-12 12:34:31 +0700

    more nvim

--- dotconfig/nvim/lua/plugins/override.lua
+++ dotconfig/nvim/lua/plugins/override.lua
@@ -34,6 +34,9 @@ return {
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp = require("cmp")
+      opts.mapping = vim.tbl_deep_extend("force", opts.mapping, {
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+      })
       opts.preselect = cmp.PreselectMode.None
       opts.completion = {
         completeopt = "menu,menuone,noinsert,noselect",
